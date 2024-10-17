c4ac4cdf087370dcea50dcb0eceef92be6c041ee
Felix Watts <felixwatts@gmail.com>
2024-10-17 16:24:17 +0100

    expose error type

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ mod err;
 mod model;
 
 pub use model::DeviceState;
+pub use crate::err::*;
 
 use record::Record;
-use crate::err::*;
 
 pub fn parse_manufacturer_data(manufacturer_data: &[u8], device_encryption_key: &[u8]) -> Result<DeviceState> {
     let record = Record::new(manufacturer_data, device_encryption_key)?;
